0205f7dd7d621294c665cd3f98f976915c2e3edf
Janne <janne.kortemaki@gmail.com>
2021-04-11 12:46:47 +0300

    JUnit + Jacoco toimii

---
+++ mielialapaivakirja/src/test/java/PatientTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import fi.janneKortemaki.logics.Patient;  
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jmjkorte
+ */
+public class PatientTest {
+    
+    public PatientTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void ConstructorCreatesNameAndDateOfBirthRight() {
+        Patient patient = new Patient("Ahtisaari", "Martti", 1921, 9, 10);
+        assertEquals("Ahtisaari, Martti, s. 1921-09-10", patient.toString());
+    }
+}
