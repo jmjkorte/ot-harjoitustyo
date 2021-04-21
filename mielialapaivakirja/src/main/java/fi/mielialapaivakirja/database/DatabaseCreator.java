db0d3e8c8222204fd06bafe9f6edd66408c44766
Janne <janne.kortemaki@gmail.com>
2021-04-22 00:04:55 +0300

    Luotu luokka DatabaseCreator ja rajapinta DAO

---
+++ mielialapaivakirja/src/main/java/fi/mielialapaivakirja/database/DatabaseCreator.java
@@ -0,0 +1,23 @@
+
+package fi.mielialapaivakirja.database;
+import java.sql.*;
+import java.util.*;
+
+public class DatabaseCreator {
+    private Scanner scanner;
+    final Connection db;
+    public DatabaseCreator() throws SQLException {
+        this.scanner = new Scanner(System.in);
+        this.db = DriverManager.getConnection("jdbc:sqlite:Patients.db");
+        
+    }
+    
+    public void createDatabase() throws SQLException{
+        Statement s = db.createStatement();
+        s.execute("CREATE TABLE Patients(id INTEGER PRIMARY KEY, firstname TEXT, surname TEXT, username TEXT UNIQUE, dateOFBirth DATE");
+        s.execute("CREATE TABLE Indicators(id INTEGER PRIMARY KEY, indicator TEXT, minvalue INTEGER, maxvalue INTEGER, patient_id INTEGER REFERENCES Patients");
+        s.execute("CREATE TABLE Entries(id INTEGER PRIMARY KEY, value INTEGER, date DATE, patient_id REFERENCES Patients, indicator_id REFERENCES Indicators");
+        s.execute("PRAGMA foreign_keys=ON");
+    }
+    
+}
---
+++ mielialapaivakirja/src/main/java/fi/mielialapaivakirja/logics/Dao.java
@@ -0,0 +1,15 @@
+
+package fi.mielialapaivakirja.logics;
+import java.sql.*;
+import java.util.*;
+        
+
+public interface Dao<T, K> {
+    void create(T object) throws SQLException;
+    T read(K key) throws SQLException;
+    T update(T object) throws SQLException;
+    void delete(K key) throws SQLException;
+    List<T> list() throws SQLException; 
+    
+    
+}
